4240eeb9dfa760e2a0ce6a8fe72f45e14bbedf34
Jordane Giuly <jordane.giuly@gmail.com>
2016-03-10 11:05:14 +0100

    add nvd3

--- mydashboard/packages/default/widgets/timer/timer.js
+++ mydashboard/packages/default/widgets/timer/timer.js
@@ -1,5 +1,3 @@
-var ical = require('ical');
-
 widget = {
   //runs when we receive data from the job
   onData: function (el, data) {
@@ -7,9 +5,35 @@ widget = {
     //The parameters our job passed through are in the data object
     //el is our widget element, so our actions should all be relative to that
     if (data.title) {
-      $('h2', el).text(data.title);
+        $('h2', el).text(data.title);
     }
 
-    $('.content', el).html(data.text);
+    var testdata = [
+        {key: "One", y: 5, color: "#5F5"},
+        {key: "Two", y: 2},
+        {key: "Three", y: 9},
+        {key: "Four", y: 7},
+        {key: "Five", y: 4},
+        {key: "Six", y: 3},
+        {key: "Seven", y: 0.5}
+    ];
+
+    var height = 300;
+    var width = 400;
+
+    nv.addGraph(function() {
+        var chart = nv.models.pieChart()
+            .x(function(d) { return d.key })
+            .y(function(d) { return d.y })
+            .width(width)
+            .height(height);
+        d3.select("#test1")
+            .datum(testdata)
+            .attr('width', width)
+            .attr('height', height)
+            .transition().duration(1200)
+            .call(chart);
+        return chart;
+    });
   }
 };
